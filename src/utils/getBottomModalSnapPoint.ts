5aeeb2e607ec52c2fe0b51beb1b74f38995059f6
hryhoriiK97 <gregory.moskaliuk@tdsoft.pl>
2023-05-27 21:24:25 +0200

    chore(app): increase bottom modal snap point in util

--- src/utils/getBottomModalSnapPoint.ts
+++ src/utils/getBottomModalSnapPoint.ts
@@ -3,11 +3,11 @@ export const getBottomModalSnapPoint = (
 ): string => {
   if (!descriptionLength) return "20%";
   if (descriptionLength <= 180) {
-    return "35%";
-  } else if (descriptionLength <= 450) {
     return "45%";
+  } else if (descriptionLength <= 450) {
+    return "55%";
   } else if (descriptionLength <= 820) {
-    return "60%";
+    return "75%";
   } else if (descriptionLength <= 1020) {
     return "90%";
   } else {
