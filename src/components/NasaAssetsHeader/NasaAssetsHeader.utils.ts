92196908ef0cc3e0f6cba8d8a987d61ec436c113
hryhoriiK97 <gregory.moskaliuk@tdsoft.pl>
2023-06-23 12:12:31 +0200

    chore(app):  improve logic for showing/hidding NasaAssetsHeader

--- src/components/NasaAssetsHeader/NasaAssetsHeader.utils.ts
+++ src/components/NasaAssetsHeader/NasaAssetsHeader.utils.ts
@@ -12,11 +12,8 @@ export const useNasaAssetsHeader = (scrollY: Animated.SharedValue<number>) => {
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (event) => {
       const currentScrollY = event.contentOffset.y;
-      // if (currentScrollY < 0) {
-      //   headerY.value = withTiming(-NASA_ASSETS_HEADER_HEIGHT);
-      // }
       if (
-        (currentScrollY <= 0 && scrollY.value <= 0) ||
+        (currentScrollY < 0 && scrollY.value <= 0) ||
         currentScrollY <= scrollY.value
       ) {
         // User is scrolling up
